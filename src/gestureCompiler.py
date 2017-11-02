a088870ca3bd18d8c60b7a5b8d7b47d13bb3aa66
Owen Levin <levin453@umn.edu>
2017-11-02 03:54:06 -0500

    gesture compiler update

--- src/gestureCompiler.py
+++ src/gestureCompiler.py
@@ -1,7 +1,28 @@
 import rospy
 from std_msgs.msg import Int32
+from numpy import dot
 
-current_scope = ["Idle"] # will be used as a stack to push and pop scopes.
+# Index of Gestures
+numbers = [0,1,2,3,4,5,6,7,8,9,14,15]
+Run = 10
+END = 11
+RecordMacro = 12
+CallMacro = 13
+Digit = 14
+Negate = 15
+Repeat = 16
+MoveX = 17
+MoveY = 18
+MoveZ = 19
+Wait = 20
+NOP = 21
+
+previous_gesture = 11
+current_macro_stack = []
+scope_blocks = []	# content of the scope
+current_block = []	# in progress construction of Block class  [String of type, number], type can be any of Run, Call, MoveX, MoveY, MoveZ, Wait
+digits = [1]
+scope = ["Idle"] # will be used as a stack to push and pop scopes.
 						 # Possible scopes:	"Idle", "Recording", "Number", "Repeating"
 legal_gestures_in_scope = {	# Each scope is mapped to a dictionary containing gestures allowed after the previous input
 	"Idle":			idle_legal_gestures,
@@ -51,7 +72,8 @@ repeating_legal_gestures = {
 	20:	[11,14],	# 20 is the wait command. (Expects a number (of seconds) to wait next
 	}
 def getLegalGestures():
-	global previous_gesture, current_scope
+	global previous_gesture
+	global current_scope
 	legal_gestures = legal_gestures_in_scope.get[current_scope]
 	return legal_gestures[previous_gesture]
 
@@ -74,13 +96,100 @@ if __name__ == '__main__':
 
 
 def callback(gesture):
+	global scope
 	current_gesture = gesture.data
+	if current_gesture == NOP:	# 21 is a No op
+		return
 	legal_gestures = getLegalGestures()
+	if current_gesture not in legal_gestures:
+		return
+	if current_gesture in numbers
+		numberHandler(current_gesture)
+	if current_gesture == RUN:
+	if current_gesture == END:
+		endHandler()
+	if current_gesture == RECORD:
+		scope.append("Recording")
+		# something with current macro stack/block
 	
+	# Add stuff here!
+
+
+##############################
+#
+#	NOT DONE AT ALL
+#	Takes care of what happens when a scope ends or an action is cancelled.
+#	and alters the current number in memory, "digits", accordingly
+#
+###############################		
+def endHandler():
+	global scope
+	global digits
+	global previous_gesture
+	global scope_blocks
+	global current_block
+	if previous_gesture in numbers
+		scope.pop()
+		num = digitsToNumber()
+		if scope[-1] == "Recording":
+			# FILL THIS IN
+			# check the current block
+		if scope[-1] == "Repeating":
+			# FILL THIS IN, etc.
+	# other cases too!		
+	if previous_gesture in [12,16]: # cancelling record/repeat command and ends the scope they started
+		scope.pop()
+	# elif: # previous_gesture in [10,13,17,18,19,20]: # cancels a run/macro call/movement/wait command
+	previous_gesture = END	
+##############################
+#
+#	takes in a gesture of either place value, *digit*, or negation, 
+#	and alters the current number in memory, "digits", accordingly
+#
+###############################
+def numberHandler(gesture):
+	global scope
+	global digits
+	if (gesture == Digit) and (scope[-1] != "Number"):	# Handle Digit if necessary
+		scope.append("Number")	# If this is a new number, add a number scope and initialize the digit list. If it's not new, we can move on to the next gesture.
+		digits = [1]			# 0th element of digit list is multiplier for the remainder of the digits. It is always +1 or -1.								
+	elif gesture == Negate:		# Handle 'negation'
+		digits[0] == -1*digits[0] # swaps sign on current_digits
+	else:	# Handle when gesture is a place value for a digit (0-9)
+		digits.append(gesture)
+	previous_gesture = gesture	# update previous gesture
+	return
 	
+##############################
+#
+#	Converts digits to the actual number it represents
+#	-There's probably a more elegant way to do this.  
+#   -One alternative is to store the digits in a string and concatenate, 
+#	 then convert the string directly to an integer and multiply by the sign...
+#
+###############################
+def digitsToNumber():	
+	global digits
+	sign = digits[0]
+	length = len(digits)-1
+	if length not 0:
+		powers = [10**(length-n-1) for n in range(0,length)]
+		num = sign *sum([digits[n]*powers[n-1] for n in range(1,length+1)])
+		return num
+	else:
+		return 0
 	
-	
-	
-	
-def possibleNextGestures(gesture):
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	
