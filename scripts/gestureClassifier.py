8e10f90cde7e22bce7fff6a4fdbec8a487c20b5e
DennisMelamed <hal.melt1@gmail.com>
2017-11-01 23:45:06 -0500

    updated gesture classifier to build the pickle file with the right class number for the gesture

--- scripts/gestureClassifier.py
+++ scripts/gestureClassifier.py
@@ -6,6 +6,32 @@ import glob
 import os
 import time
 
+
+gesture_class_nums = {  "0":0,
+                        "1":1,
+                        "2":2,
+                        "3":3,
+                        "4":4,
+                        "5":5,
+                        "6":6,
+                        "7":7,
+                        "8":8,
+                        "9":9,
+                        "run":10,
+                        "end":11,
+                        "record":12,
+                        "call":13,
+                        "digit":14,
+                        "negate":15,
+                        "repeat":16,
+                        "x":17,
+                        "y":18,
+                        "z":19,
+                        "wait":20,
+                        "nop":21 }
+
+
+
 # How many neighbors do happy villagers have?
 n_neighbors = 5
 
@@ -27,15 +53,18 @@ print pickled_gestures
 class_num = 0
 dict_class_gesture = {}
 for pickled_gesture in pickled_gestures:
+        gesture_name = os.path.splitext(os.path.basename(pickled_gesture))[0]
+        gesture_name = gesture_name[:-7]
+        print gesture_name
 	dict_class_gesture.update({class_num:pickled_gesture})
 	with open(pickled_gesture, 'r+b') as f_handler:
 		new_gesture_data = pickle.load(f_handler)			#	numpy array
 	if class_num == 0:
 		gesture_data = new_gesture_data
-		class_data = np.full((1,gesture_data.shape[0]),class_num)
+		class_data = np.full((1,gesture_data.shape[0]),gesture_class_nums[gesture_name])
 	else:
 		gesture_data = np.vstack((gesture_data, new_gesture_data)) 
-		class_data = np.hstack((class_data, np.full((1,new_gesture_data.shape[0]),class_num)))
+		class_data = np.hstack((class_data, np.full((1,new_gesture_data.shape[0]),gesture_class_nums[gesture_name])))
 	class_num = 1 + class_num
 
 # output a dictionary of what labels correspond to what gestures
