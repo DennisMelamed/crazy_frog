6ca2c9f60ae2cb39b2ca2d8905a2464ba3692300
DennisMelamed <hal.melt1@gmail.com>
2017-11-08 15:18:13 -0600

    tabs

--- src/GUI.py
+++ src/GUI.py
@@ -6,7 +6,7 @@ from gestureClasses import *
 from gestureInfo import *
 
 def getData():
-	rospy.init_node('crazyFrog/GUI_data_reciever', anonymous=True)
+    rospy.init_node('crazyFrog/GUI_data_reciever', anonymous=True)
     sub1 = rospy.Subscriber('crazyFrog/current_gesture', Int32 , DO-STUFF1)
     sub2 = rospy.Subscriber('crazyFrog/current_program', MacroRequest, DO-STUFF2)
     sub3 = rospy.Subscriber('crazyFrog/compiler_data', CompilerData, DO-STUFF3)
