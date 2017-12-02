f7bd1f2e8cb09ae62d8a1bdca0ed1587dfba384f
DennisMelamed <hal.melt1@gmail.com>
2017-12-02 16:18:38 -0600

    runtime works for spatial motions, in the middle of implementing the wait capability

--- src/runtime.cpp
+++ src/runtime.cpp
@@ -19,7 +19,7 @@
 #include <string>
 
 #define POSITION_ERROR .1
-enum Direction{x, y, z, w};
+enum Direction{w, x, y, z};
 
 struct Command
 {
@@ -37,14 +37,19 @@ struct Position
 geometry_msgs::PoseStamped msg;
 std::queue <Command> command_queue;
 int current_program_number = 0;
+int wait_timer = 0;
 
 void addMacroCommandsToQueue(int macro_number)
 {
-	char macro_num = (char) macro_number;
+	char macro_num[3];
+	sprintf(macro_num,"%d",macro_number); 
+	ROS_INFO("macro number int: %d, macro number char: %c", macro_number, macro_num[0]);
 	std::string macro_n("../macros/macro");
-        macro_n	+= &macro_num;
-	macro_n +=".csv";
+        macro_n.append(1, macro_num[0]);
+	macro_n.append(".csv");
 	
+	ROS_INFO("adding from file: %s" , macro_n.c_str());
+
 	std::ifstream macro_file(macro_n.c_str());
 	std::string line;
 	while (std::getline(macro_file, line))
@@ -53,11 +58,14 @@ void addMacroCommandsToQueue(int macro_number)
 		char a;
 		int b;
 		if (!(iss >> a >> b)) { break; } // error
+		ROS_INFO("command added: %c, distance: %d", a, b);
 		Command new_command;
-		new_command.a = static_cast<Direction>(a-120);
+		new_command.a = static_cast<Direction>(a-119);
+		ROS_INFO("added command with direction: %d", new_command.a);
 		new_command.amount = b;
 		command_queue.push(new_command);
 	}
+	
 }
 
 void programCallback(const crazyFrog::MacroRequest& request)
@@ -66,7 +74,9 @@ void programCallback(const crazyFrog::MacroRequest& request)
 	{
 		current_program_number +=1;
 		addMacroCommandsToQueue(request.macro_number);
+		ROS_INFO("adding a new program to queue");
 	}
+	ROS_INFO("currently running program # %d, which is macro # %d", current_program_number, request.macro_number);
 }
 
 void updateTransform(tf::StampedTransform& transform, tf::TransformListener& listener)
@@ -80,6 +90,8 @@ void updateTransform(tf::StampedTransform& transform, tf::TransformListener& lis
 		ROS_ERROR("%s",ex.what());
 		ros::Duration(1.0).sleep();
 	}
+	
+//	ROS_INFO("current transform x: %f y %f z %f", transform.getOrigin().x(), transform.getOrigin().y(), transform.getOrigin().z());
 }
 
 
@@ -88,8 +100,7 @@ void init(tf::StampedTransform& transform)
 	start_position.x = transform.getOrigin().x();
 	start_position.y = transform.getOrigin().y();
 	start_position.z = transform.getOrigin().z();
-
-	ROS_INFO("current transform x: %f y %f z %f", transform.getOrigin().x(), transform.getOrigin().y(), transform.getOrigin().z());
+//ROS_INFO("current transform x: %f y %f z %f", transform.getOrigin().x(), transform.getOrigin().y(), transform.getOrigin().z());
 
 }
 
@@ -110,13 +121,18 @@ bool posWithinError(double start_pos, double current_pos, double goal_pos, doubl
 bool goalAchieved(tf::StampedTransform& transform)
 {
 	ROS_INFO("current transform x: %f y %f z %f", transform.getOrigin().x(), transform.getOrigin().y(), transform.getOrigin().z());
-
+	ROS_INFO("wait_timer: %d ", wait_timer);
    	if(  posWithinError(start_position.x, transform.getOrigin().x(), msg.pose.position.x, POSITION_ERROR) &&
    	     posWithinError(start_position.y, transform.getOrigin().y(), msg.pose.position.y, POSITION_ERROR) &&
-   	     posWithinError(start_position.z, transform.getOrigin().z(), msg.pose.position.z, POSITION_ERROR))
+   	     posWithinError(start_position.z, transform.getOrigin().z(), msg.pose.position.z, POSITION_ERROR) &&
+	     wait_timer ==0)
    	{
    	     return true;
    	}
+	else
+	{
+		wait_timer -= 1;
+	}
    	return false;
 
 }
@@ -126,11 +142,22 @@ void updateMessage(tf::StampedTransform& transform)
 	
       if(!command_queue.empty() && goalAchieved(transform))
       { 
+	      ROS_INFO("setting new goal pos");
 		current = command_queue.front();
 		command_queue.pop();
-	  	msg.pose.position.x = (current.a == x) ? (msg.pose.position.x + current.amount) : (msg.pose.position.x);	
-	  	msg.pose.position.y = (current.a == y) ? (msg.pose.position.y + current.amount) : (msg.pose.position.y);	
-	  	msg.pose.position.z = (current.a == z) ? (msg.pose.position.z + current.amount) : (msg.pose.position.z);	
+		ROS_INFO("setting new goal position with direction: %d, at point: %f", current.a, msg.pose.position.x+current.amount);
+		Direction a = x;
+		Direction b = y;
+		Direction c = z;
+		Direction d = w;
+	  	msg.pose.position.x = (current.a == a) ? (msg.pose.position.x + current.amount) : (msg.pose.position.x);	
+	  	msg.pose.position.y = (current.a == b) ? (msg.pose.position.y + current.amount) : (msg.pose.position.y);	
+	  	msg.pose.position.z = (current.a == c) ? (msg.pose.position.z + current.amount) : (msg.pose.position.z);	
+		if(current.a == d)
+		{
+			wait_timer = current.amount*10;
+		}
+		ROS_INFO("actual goal pose set: %f, %f, %f", msg.pose.position.x, msg.pose.position.y, msg.pose.position.z);
 	  	// handle (current.a == w) // a wait command
 		init(transform);
       }
@@ -157,11 +184,14 @@ int main(int argc, char **argv)
 
    	ros::Rate rate(10);
    	
+	msg.pose.position.x = start_position.x;
+	msg.pose.position.y = start_position.y;
+	msg.pose.position.z = start_position.z;
 
    	while(ros::ok())
    	{
    		updateTransform(transform, listener);
-   	    updateMessage(transform);
+   		updateMessage(transform);
    	   	pub.publish(msg);
    		rate.sleep();
    	}
